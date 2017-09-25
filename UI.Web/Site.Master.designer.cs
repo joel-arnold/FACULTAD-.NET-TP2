e5a16fe525721c95926554026bd312fb04df7a18
joel-arnold <ar.joelarnold@yahoo.com.ar>
2017-09-25 19:47:10 -0300

    FALTA: Login escritorio, AMBC planes (web), otro ABMC para escritorio y terminar login web.

--- UI.Web/Site.Master.designer.cs
+++ UI.Web/Site.Master.designer.cs
@@ -1,6 +1,6 @@
 ﻿//------------------------------------------------------------------------------
 // <generado automáticamente>
-//     Este código fue generado por una herramienta.wdwedwedwedwesaf
+//     Este código fue generado por una herramienta.
 //
 //     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
 //     se vuelve a generar el código. 
