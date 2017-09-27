28d9d027d099e6f1b2b939e27a588fe1e94235d7
joel-arnold <ar.joelarnold@yahoo.com.ar>
2017-09-27 13:38:03 -0300

    Comit posterior a unir el master

--- Datos.BaseDeDatos/Adapter.cs
+++ Datos.BaseDeDatos/Adapter.cs
@@ -14,7 +14,7 @@ namespace Data.Database
 
         //Clave por defecto a utlizar para la cadena de conexion
 
-        const string consKeyDefaultCnnString = "ConnStringExpress";
+        const string consKeyDefaultCnnString = "ConnStringLocal";
 
         public SqlConnection SqlConn
         {
